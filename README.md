b8f958a549af3d0eecd70ea39e4681f2d14683b0
qdev <qurusan@gmail.com>
2020-02-28 16:01:43 +0300

    step 4

---
+++ README.md
@@ -0,0 +1,1 @@
+intainability](https://api.codeclimate.com/v1/badges/a99a88d28ad37a79dbf6/maintainability)](https://codeclimate.com/github/codeclimate/codeclimate/maintainability)
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import readlineSync from 'readline-sync';
 
-export const sayHi = () => {
+export default () => {
   const name = readlineSync.question('May I have your name?: ');
   console.log(`Hello, ${name}!`);
-}
-
+};
+// export default sayHi;
